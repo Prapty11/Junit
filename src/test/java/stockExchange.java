8952dff63ca2d9f948efa1a0c98459aa9d63285a
Israt11 <133569364+Prapty11@users.noreply.github.com>
2025-05-05 10:17:17 +0600

    Update stockExchange.java

--- src/test/java/stockExchange.java
+++ src/test/java/stockExchange.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 import java.util.List;
@@ -13,7 +14,7 @@ public class stockExchange {
 
     @BeforeAll
     public void setup(){
-        driver.get("https://dsebd.org/latest_share_price_scroll_by_value.php");
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
